b221d4a2080971cec9cc4504aa7411d0be425819
UNDERSTOOD? <sampervlad373@gmail.com>
2025-01-10 19:58:02 +0200

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Этот скрипт выводит сообщение об успешном запуске
 echo "[Init Python..]"
+sleep 2
 python3 --version
 echo "[Python Successfully initialized]"
 echo "[Checking Bash]"
+sleep 2
 bash --version | head -n 1
 echo "[Bash Checked]"
+sleep 1
 echo "[Successful Launch]"
 exit 0
