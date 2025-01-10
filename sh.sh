277a7bfa2c1bd46d78cede987c44db6d91c02af9
UNDERSTOOD? <sampervlad373@gmail.com>
2025-01-10 18:28:36 +0200

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-while true; do
-    for R in {0..255..15}; do
-        for G in {0..255..15}; do
-            for B in {0..255..15}; do
-                echo -e "\033[38;2;${R};${G};${B}mСкрипт активен!\033[0m"
-                sleep 0.1
-                clear
-            done
-        done
-    done
-done
+echo  "\033[34mСкрипт подгрузился и успешно работает\033[0m"
+
+echo  "\033[34mThe script has loaded and is working successfully\033[0m"
+
+exit 0
