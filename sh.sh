0cc77c9788e7f5e20ff07d3f16df9a9e0dfb1667
UNDERSTOOD? <sampervlad373@gmail.com>
2025-01-10 19:54:13 +0200

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Этот скрипт выводит сообщение об успешном запуске
-
-echo "[SUCCESSFUL LAUNCH]"
+echo "[Init Python..]"
+python3 --version
+echo "[Python Successfully initialized]"
+echo "[Checking Bash]"
+bash --version | head -n 1
+echo "[Bash Checked]"
+echo "[Successful Launch]"
 exit 0
