819ee91f7b1ba8ad15f7aa85bc516b46022a55a1
UNDERSTOOD? <sampervlad373@gmail.com>
2025-01-13 00:24:01 +0200

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# Скачиваем файл Автообнова.py
+FILE_NAME="Автообнова.py"
+DOWNLOAD_URL="https://raw.githubusercontent.com/merfiDEV/Z/main/$FILE_NAME"
+SAVE_PATH="/storage/emulated/0/$FILE_NAME"
+
+echo "Скачиваем файл $FILE_NAME..."
+
+if curl -o "$SAVE_PATH" -L "$DOWNLOAD_URL"; then
+    echo "Файл авто обновы установлен в $SAVE_PATH"
+    echo "Auto-update file installed at $SAVE_PATH"
+else
+    echo "Ошибка скачивания файла $FILE_NAME!"
+    exit 1
+fi
+
 clear
 echo "[DEBUG]"
 
@@ -5,7 +22,6 @@ if [ -f /sys/class/thermal/thermal_zone0/temp ]; then
     CPU_TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
     CPU_TEMP_C=$(echo "scale=1; $CPU_TEMP / 1000" | bc)
     echo "[CPU TEMP=$CPU_TEMP_C°C]"
-    # Проверяем, равна ли температура 37.0°C
     if [ "$(echo "$CPU_TEMP_C == 37.0" | bc)" -eq 1 ]; then
         echo "[CPU NOT RESPONSE]"
     fi
